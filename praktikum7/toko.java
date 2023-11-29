a99443aafebf11a7a0fc8dc978821211a7b70efd
Muhammad Rifky Akbar <114640734+MuhammadRifkyAkbar@users.noreply.github.com>
2023-11-29 19:52:14 +0700

    Tugas Pertemuan 8
    Muhammad Rifky Akbar
    2210631170088
    3C

---
+++ praktikum7/toko.java
@@ -0,0 +1,19 @@
+// Toko.java
+package praktikum7;
+
+public class toko {
+    private String[][] barang = {
+            {"a001", "a002", "a003", "a004", "a005"},
+            {"Buku", "Pensil", "Pulpen", "Tas", "Binder"}
+    };
+
+    private int[] hargaBarang = {3000, 4000, 5000, 30000, 25000};
+
+    public String[][] getBarang() {
+        return this.barang;
+    }
+
+    public int[] getHargaBarang() {
+        return this.hargaBarang;
+    }
+}
---
+++ praktikum7/tokoMain.java
@@ -0,0 +1,14 @@
+
+package praktikum7;
+
+
+public class tokoMain {
+
+    public static void main(String[] args) {
+        system pelanggan = new system();
+
+        pelanggan.input();
+        pelanggan.getStruk();
+    }
+
+}
